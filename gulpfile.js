4566dba4eb8a75a31e1d5305cb99ff9be18eb9ae
Franklin <ffffranklin@pobox.com>
2015-09-22 23:49:40 -0700

    Adding coverage, codeclimate, travis ci support and upgrading node to stable

--- gulpfile.js
+++ gulpfile.js
@@ -13,24 +13,42 @@ var jsFiles = [
 ];
 
 gulp.task('default', ['lint', 'test']);
+
 gulp.task('test', shell.task([
   'node node_modules/babel-tape-runner/bin/babel-tape-runner test/**/*.js | faucet'
 ]));
+
 gulp.task('build', ['pack:build']);
+
 gulp.task('pack:build', require('./gulp/webpack').build);
+
 gulp.task('pack:build-dev', require('./gulp/webpack').buildDev);
+
 gulp.task('pack:server', require('./gulp/webpack').server);
+
 gulp.task('serve', ['pack:server','serve:jekyll']);
+
 gulp.task('serve:jekyll', shell.task([
   'jekyll serve -w --config _config.yml,_config.dev.yml .'
 ]));
+
 gulp.task('lint', function () {
   return gulp.src(jsFiles)
     .pipe(eslint())
     .pipe(eslint.format());
 });
+
 gulp.task('watch', ['lint', 'test'], function() {
   gulp.watch(jsFiles, ['lint', 'test']);
 });
+
+gulp.task('cover', shell.task([
+  'istanbul cover babel-tape-runner -- "test/**/*.test.js"'
+]));
+
+gulp.task('codeclimate', ['cover'], shell.task([
+  'codeclimate-test-reporter < coverage/lcov.info'
+]));
+
 
 
