ab3dc66d52b0d10dc9c424458f53b25d9cad33eb
Franklin <ffffranklin@users.noreply.github.com>
2015-09-22 20:45:08 -0700

    Adding webpack build dev-server and main serve task

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,18 @@
 
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var shell = require('gulp-shell');
 var eslint = require('eslint');
 
 gulp.task('default', ['test']);
 gulp.task('test', function (cb) { cb(); });
 gulp.task('pack:build', function (cb) { cb(); });
 gulp.task('pack:build-dev', require('./gulp/webpack').buildDev);
-gulp.task('pack:server', function (cb) { cb(); });
+gulp.task('pack:server', require('./gulp/webpack').server);
+gulp.task('serve', ['pack:server','serve:jekyll']);
+gulp.task('serve:jekyll', shell.task([
+  'jekyll serve -w --config _config.yml,_config.dev.yml .'
+]));
 gulp.task('lint', function () {
   return gulp.src(['gulpfile.js', 'site/es6/**/*.js', 'test/unit/es6/**/*.js'])
     .pipe(eslint())
