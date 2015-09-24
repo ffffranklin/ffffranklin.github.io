2ae738504b6b3c94e6dd9ba83b610fe149bc5541
Franklin <ffffranklin@pobox.com>
2015-09-23 19:41:50 -0700

    Updating configuration after repo squish

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
 var path = require('path');
 module.exports = {
-  entry: path.join(__dirname, '/site/_es6/index.js'),
+  entry: path.join(__dirname, '/_es6/index.js'),
   output: {
-    path: path.join(__dirname, '/site/js'),
+    path: path.join(__dirname, '/js'),
     publicPath: 'js/',
     filename: 'bundle.js'
   },
   module: {
     loaders: [
       {
-        test:path.join(__dirname, '/site/_es6'),
+        test:path.join(__dirname, '/_es6'),
         loader: 'babel-loader'
       }
     ]
