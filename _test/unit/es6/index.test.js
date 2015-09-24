9014a057b701e0632e725e469d160d28a687a24a
Franklin <ffffranklin@pobox.com>
2015-09-23 19:43:22 -0700

    Fixing tests

--- _test/unit/es6/index.test.js
+++ _test/unit/es6/index.test.js
@@ -1,5 +1,5 @@
 var test = require('tape');
-var index = require('../../../site/_es6/index.js');
+var index = require('../../../_es6/index.js');
 test('starts', function (t) {
   t.ok(index.start);
   t.end();
