65a1573d90ceafa4c0e0369318f518573bda9542
Franklin <ffffranklin@pobox.com>
2015-12-18 01:54:06 -0800

    Moving header above all content

--- _es6/index.js
+++ _es6/index.js
@@ -3,3 +3,9 @@
 //import { pick } from 'lodash';
 
 var $ = require('jquery');
+
+var classToggler = function classToggler($el, classes) {
+  return $.fn.toggleClass.bind($el, classes);
+};
+
+$('.drawer-trigger').on('click', classToggler($('.drawer-trigger'), 'opened closed'));
