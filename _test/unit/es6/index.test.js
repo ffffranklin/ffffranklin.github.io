e1083a0812baac51218d9798df75be60fcc76461
Franklin <ffffranklin@users.noreply.github.com>
2015-12-20 04:06:39 -0800

    Upgrade travis requirements and exposing class toggler

--- _test/unit/es6/index.test.js
+++ _test/unit/es6/index.test.js
@@ -1,6 +1,15 @@
+'use strict';
+
 var test = require('tape');
 var index = require('../../../_es6/index.js');
+
 test('main function', function (t) {
   t.equal(typeof index.__main__, 'function');
   t.end();
+});
+
+test('class toggler returns handler', function (assert) {
+  var result = index.classToggler({}, 'open closed');
+  assert.equal(typeof result, 'function')
+  assert.end();
 });
