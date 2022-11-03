1759edcbaead92d24375ff67a0fa685da3eefc2f
Franklin Clark <ffffranklin@pobox.com>
2022-11-03 02:53:35 -0700

    Fix logo css

--- components/logo-flip.tsx
+++ components/logo-flip.tsx
@@ -1,44 +1,25 @@
+import styles from './logo-flip.module.scss';
+import {useState} from "react";
 /**
  *
  * @constructor
  */
 export function LogoFlip() {
-  let dom: Document | undefined;
+  const [isActive, setActive] = useState(false);
 
-  if (typeof window !== 'undefined') {
-    dom = window.document;
-  }
-
-  (function(dom) {
-    if (!dom) return;
-    const logo = dom.querySelector('.long-shadow-logo');
-
-    const toggleClass = function toggleClass(className, newClass) {
-      if (className.search(newClass) !== -1) {
-        return className.replace(newClass, '').replace('  ', ' ');
-      } else {
-        return [className, newClass].join(' ');
-      }
-    };
-
-    const handleHover = function handleHover() {
-      // eslint-disable-next-line no-invalid-this
-      this.className = toggleClass(this.className, 'hover');
-    };
-
-    logo.addEventListener('mouseenter', handleHover);
-    logo.addEventListener('mouseleave', handleHover);
-  })(dom);
+  const handleToggle = () => {
+    setActive(!isActive);
+  };
 
   return (
-    <div className="long-shadow-logo">
-      <div className="flipper">
-        <div className="front content">
+    <div className={[styles.longShadowLogo, isActive ? "hover" : null].join(' ')} onMouseEnter={handleToggle} onMouseLeave={handleToggle}>
+      <div className={styles.flipper}>
+        <div className={[styles.front, 'content'].join(' ')}>
           <a href="https://github.com/ffffranklin" target="_blank" rel="noreferrer">
             <img alt="Franklin Clark Logo" src="img/logo-ffff-shadow.svg"/>
           </a>
         </div>
-        <div className="back content">
+        <div className={[styles.back, 'content'].join(' ')}>
           <a href="https://github.com/ffffranklin" target="_blank" rel="noreferrer">
             <svg
               version="1.1"
@@ -63,7 +44,6 @@ export function LogoFlip() {
                   xlinkHref="https://avatars2.githubusercontent.com/u/1546561?v=3&amp;s=110"></image>
               </g>
             </svg>
-
           </a>
         </div>
       </div>
