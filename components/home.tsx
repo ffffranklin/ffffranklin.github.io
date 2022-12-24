7dfc47ec74f3ad0454038674b7315fd616f51232
Franklin Clark <ffffranklin@pobox.com>
2022-12-24 00:26:47 -0800

    Fix mobile content and look and feel

--- components/home.tsx
+++ components/home.tsx
@@ -12,11 +12,9 @@ export function Home() {
           <h1>Franklin Clark</h1>
           <p className="content">
             Looks like you're a little early! My site is still being built. In
-            the meantime check out my work on
-            <a href="https://github.com/ffffranklin" target="_blank" rel="noreferrer">GitHub</a> or
-            <a href="http://codepen.io/ffffranklin" target="_blank" rel="noreferrer">CodePen</a>
-            and follow me on
-            <a href="https://twitter.com/_ffffranklin" target="_blank" rel="noreferrer">Twitter</a>.
+            the meantime check out my work
+            on <a href="https://github.com/ffffranklin" target="_blank" rel="noreferrer">GitHub</a> or <a href="http://codepen.io/ffffranklin" target="_blank" rel="noreferrer">CodePen</a> and
+            follow me on <a href="https://twitter.com/_ffffranklin" target="_blank" rel="noreferrer">Twitter</a>.
           </p>
         </div>
       </section>
