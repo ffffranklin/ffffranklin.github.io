c8b04f7cab11d5128eb80586f17d8fea5d090016
Franklin Clark <ffffranklin@pobox.com>
2024-06-22 21:22:45 -0700

    Rewrite keynase asset request

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   output: "export",
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/keybase.txt',
+        destination: '/.well-known/keybase.txt',
+      }
+    ]
+  }
 };
 
 module.exports = nextConfig;
