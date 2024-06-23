eb3d0b090d8f71675603fd90f4287b63a5a51621
Franklin Clark <ffffranklin@pobox.com>
2024-06-22 21:28:00 -0700

    Remove invalid config

--- next.config.js
+++ next.config.js
@@ -2,14 +2,6 @@
 const nextConfig = {
   output: "export",
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/keybase.txt',
-        destination: '/.well-known/keybase.txt',
-      }
-    ]
-  }
 };
 
 module.exports = nextConfig;
