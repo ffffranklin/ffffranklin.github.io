40683579c4363d697856fc4abd78b590c256404f
Franklin Clark <ffffranklin@users.noreply.github.com>
2025-06-30 15:24:02 -0700

    Fix content font

--- app/info/resume/page.tsx
+++ app/info/resume/page.tsx
@@ -1,8 +1,12 @@
 import Resume from '../../../markdown/resume.mdx';
 import styles from './page.module.scss';
+import React from "react";
 
 export default function Page() {
     return <>
+      <style>
+        {'@page { size: auto; margin: 10mm 10mm 10mm 10mm; color:red}'}
+      </style>
       <article className={styles.resume}><Resume/></article>
     </>;
 }
