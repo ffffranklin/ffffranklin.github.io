4615d74096608b2918201629cf88b9ee4fee2da1
Franklin Clark <ffffranklin@users.noreply.github.com>
2025-06-29 20:43:52 -0700

    Wrap mdx resume content

---
+++ app/info/resume/page.tsx
@@ -0,0 +1,6 @@
+import Resume from '../../../markdown/resume.mdx';
+import styles from './page.module.scss';
+
+export default function Page() {
+    return <article className={styles.resume}><Resume/></article>;
+}
